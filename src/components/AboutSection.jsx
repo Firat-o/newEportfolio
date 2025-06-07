8f66b24c64768e44a2d58afda3a46d7d59182c7d
Firato <142419225+Firat-o@users.noreply.github.com>
2025-06-07 09:37:46 +0200

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -30,6 +30,7 @@ export const AboutSection = () => {
               <a
                 href="/projects/Firato_CV.pdf" 
                 download="Firat_Resume.pdf"
+                target="_blank"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Lebenslauf
