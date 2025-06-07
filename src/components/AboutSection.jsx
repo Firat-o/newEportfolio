2d16e60af80c9f181de2a156cb61976aba4f0ae6
Firato <142419225+Firat-o@users.noreply.github.com>
2025-06-07 09:26:35 +0200

    Update AboutSection.jsx
    resume added

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -28,7 +28,7 @@ export const AboutSection = () => {
                 Nachricht senden
               </a>
               <a
-                href="/Firato_CV.pdf"
+                href="public/resume-software-dev.pdf"
                 download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
