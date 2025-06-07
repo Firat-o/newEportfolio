e8bb793e1ed279f6812519d5811e68aefbf3d475
Firato <142419225+Firat-o@users.noreply.github.com>
2025-06-07 09:33:51 +0200

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,5 +1,4 @@
 import { Briefcase, Code, User } from "lucide-react";
-import resumePDF from '../../assets/resume-software-dev.pdf';
 
 export const AboutSection = () => {
   return (
@@ -29,7 +28,7 @@ export const AboutSection = () => {
                 Nachricht senden
               </a>
               <a
-                href={resumePDF}
+                href="/projects/Firato_CV.pdf" 
                 download="Firat_Resume.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
